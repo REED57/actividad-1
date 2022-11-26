a42e4aafc4fbdcdfac2848fc485274b09ba6777f
de337 <de337@TUFDANNY15>
2022-11-25 18:40:46 -0600

    actividad 1

---
+++ src/main/java/com/mycompany/repositori2/Repositori2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.repositori2;
+
+/**
+ *
+ * @author de337
+ */
+public class Repositori2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("DANIEL ");
+        System.out.println("ERICK ");
+        //System.out.println("HERBER");
+        //System.out.println("ALEX");
+    }
+}
